7532cbe584d070032abf0d076d3a651f42a28d27
Art <surasak.jaikum@mail.kmutt.ac.th>
2020-08-11 12:31:29 +0700

    Updates

---
+++ precache-manifest.7164cc3145232b839b572943dfc07e49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3d01bf1dd73c638f74c4faca498e80b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5b74447b20bc2c0cc088",
+    "url": "./static/css/main.cdbdece8.chunk.css"
+  },
+  {
+    "revision": "41f3ce433d9c9ab5d1fc",
+    "url": "./static/js/2.a25a797c.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "./static/js/2.a25a797c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b74447b20bc2c0cc088",
+    "url": "./static/js/main.a80a42bc.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.a80a42bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "269262206e66530cca65",
+    "url": "./static/js/runtime-main.14d32586.js"
+  }
+]);
